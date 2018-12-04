033158a041aa132b775594fda08bf199e4c5d71c
jgrss <graesser@bu.edu>
2018-12-04 14:02:44 +1000

    added version

--- zones/__init__.py
+++ zones/__init__.py
@@ -1,4 +1,5 @@
 from .raster_stats import RasterStats
 from .testing.test_single_band import test_01
+from .version import __version__
 
-__all__ = ['RasterStats', 'test_01']
+__all__ = ['RasterStats', 'test_01', '__version__']
