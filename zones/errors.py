17d54eb1a90ccaeb3a6f69b05b4a728199600f80
Jordan <graesserj@athena52.cm.cluster>
2020-01-15 09:25:20 +1000

    modified log file writing

--- zones/errors.py
+++ zones/errors.py
@@ -1,11 +1,13 @@
+import os
 import logging
+
 
 _FORMAT = '%(asctime)s:%(levelname)s:%(lineno)s:%(module)s.%(funcName)s:%(message)s'
 _formatter = logging.Formatter(_FORMAT, '%H:%M:%S')
 _handler = logging.StreamHandler()
 _handler.setFormatter(_formatter)
 
-logging.basicConfig(filename='zones.log',
+logging.basicConfig(filename=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'zones.log'),
                     filemode='w',
                     level=logging.DEBUG)
 
