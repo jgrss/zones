224b6d0d57416a8be2c321bfad7366eba0fcbf0c
Jordan <graesserj@athena52.cm.cluster>
2020-01-22 13:15:34 +1000

    added util module and GDAL 2/3 axis check

---
+++ zones/util.py
@@ -0,0 +1,17 @@
+from osgeo import osr
+
+
+def check_axis_order(spatial_reference):
+
+    """
+    Checks for GDAL 2/3 spatial reference axis order
+
+    Args:
+        spatial_reference (object): The spatial reference object to check.
+
+    Returns:
+        None
+    """
+
+    if hasattr(sr, 'SetAxisMappingStrategy'):
+        spatial_reference.SetAxisMappingStrategy(osr.OAMS_TRADITIONAL_GIS_ORDER)
