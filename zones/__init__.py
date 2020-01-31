d6f20734d831b2f679eba79bac0fdd3e6c29ba95
Jordan <graesserj@athena52.cm.cluster>
2020-01-31 13:40:44 +1000

    added relative import for grid() function

--- zones/__init__.py
+++ zones/__init__.py
@@ -1,7 +1,14 @@
 from .point_stats import PointStats
 from .raster_stats import RasterStats
 from .testing.test import test_raster
+from .base import grid
 from .base import voronoi
 from .version import __version__
 
-__all__ = ['PointStats', 'RasterStats', 'voronoi', 'test_raster', '__version__']
+
+__all__ = ['PointStats',
+           'RasterStats',
+           'grid',
+           'voronoi',
+           'test_raster',
+           '__version__']
