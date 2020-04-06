cc496ecf1ede4850867a48912c502a4d5a51c6e7
jgrss <graesser@bu.edu>
2020-04-06 15:40:39 +1000

    added check for pyproj.CRS instances

--- zones/util.py
+++ zones/util.py
@@ -17,12 +17,12 @@ def check_crs(crs):
         ``str`` as WKT
     """
 
-    if isinstance(crs, pyproj.crs.crs.CRS) or isinstance(crs, pyproj.crs.CRS):
+    if isinstance(crs, pyproj.crs.crs.CRS) or isinstance(crs, pyproj.crs.CRS) or isinstance(crs, pyproj.CRS):
 
-        if crs.is_geographic:
-            dst_crs = CRS.from_wkt("""GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]""")
-        else:
-            dst_crs = CRS.from_wkt(crs.to_wkt())
+        # if crs.is_geographic:
+        #     dst_crs = CRS.from_wkt("""GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]""")
+        # else:
+        dst_crs = CRS.from_epsg(crs.to_epsg())
 
     elif isinstance(crs, CRS):
         dst_crs = crs
