6c95ed9fbcdbe4dc0b3e9b8c679627235758940b
jgrss <graesser@bu.edu>
2020-04-30 07:08:11 +1000

    added check for pyproj.CRS objects withi empty EPSG code

--- zones/util.py
+++ zones/util.py
@@ -22,7 +22,11 @@ def check_crs(crs):
         # if crs.is_geographic:
         #     dst_crs = CRS.from_wkt("""GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]""")
         # else:
-        dst_crs = CRS.from_epsg(crs.to_epsg())
+
+        try:
+            dst_crs = CRS.from_epsg(crs.to_epsg())
+        except:
+            dst_crs = check_crs(crs.to_wkt())
 
     elif isinstance(crs, CRS):
         dst_crs = crs
